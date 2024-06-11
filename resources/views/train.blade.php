c44227827e05db77bf03493321dd0b615bde94ec
wmancaniello <wmancaniello@gmail.com>
2024-06-11 22:18:21 +0200

    print information

--- resources/views/train.blade.php
+++ resources/views/train.blade.php
@@ -4,6 +4,7 @@
     <table class="table">
         <thead>
             <tr>
+                <th>ID</th>
                 <th>Azienda</th>
                 <th>Stazione di Partenza</th>
                 <th>Stazione di Arrivo</th>
@@ -16,14 +17,22 @@
             </tr>
         </thead>
 
-        {{-- fare un forEach --}}
         <tbody>
-            <tr>
-                <th scope="row">example</th>
-                <td>example</td>
-                <td>example</td>
-                <td>example</td>
-            </tr>
+            {{-- fare un forEach --}}
+            @foreach ($trainInfo as $train)
+                <tr>
+                    <th scope="row">{{ $train->id }}</th>
+                    <td>{{ $train->company }}</td>
+                    <td>{{ $train->departure_station }}</td>
+                    <td>{{ $train->arrival_station }}</td>
+                    <td>{{ $train->departure_time }}</td>
+                    <td>{{ $train->arrival_time }}</td>
+                    <td>{{ $train->train_code }}</td>
+                    <td>{{ $train->number_of_carriages }}</td>
+                    <td>{{ $train->on_time ? 'Si' : 'No' }}</td>
+                    <td>{{ $train->cancelled ? 'Si' : 'No' }}</td>
+                </tr>
+            @endforeach
         </tbody>
     </table>
 @endsection
